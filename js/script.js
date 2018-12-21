681944aa8b550112b2a86bfa74d30fa040148dec
Romvet <v.romanenko@inspire.kiev.ua>
2018-12-21 23:08:20 +0200

    Добавил анимацию форме

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 var clickbutton= document.querySelector(".button-search");
 var modalform=document.querySelector(".modal-search");
+var checkin=modalform.querySelector("[name=data-check-in]");
+
+
+
 
 clickbutton.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalform.classList.toggle("modal-show");
-})
+  })
 
 
