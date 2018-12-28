42f971fd62764287c835392bb2b605fbef726470
Romvet <v.romanenko@inspire.kiev.ua>
2018-12-28 20:35:53 +0200

    Добавил local storage

--- js/script.js
+++ js/script.js
@@ -7,14 +7,41 @@ var checkout=modalform.querySelector("[name=date-check-out]");
 var numberpeople=modalform.querySelector("[name=number-people]");
 var numberchild=modalform.querySelector("[name=number-child]");
 
+var isStorageSupport = true;
+var storage = "";
+
+try{
+  storage = localStorage.getItem("checkin");
+} catch (err){
+  isStorageSupport = false;
+}
+
  modalform.classList.add("modal-hid");
 
-clickbutton.addEventListener("click", function (evt) {
+ clickbutton.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalform.classList.toggle("modal-hid");
    modalform.classList.toggle("modal-show");
+  if (storage){
+    checkin.value = storage;
+    checkout.focus();
+  } else{
     checkin.focus();
-   })
+  }
+})
+
+
+form.addEventListener("submit", function (evt) {
+if(!checkin.value || !checkout.value || !numberpeople.value || !numberchild.value){
+evt.preventDefault();
+console.log("Заполните пустые поля");
+  } else{
+    if (isStorageSupport){
+    localStorage.setItem("checkin", login.value);
+    }
+  }
+});
+
 
 
 
