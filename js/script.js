6a08c695d137c2a0e7ff3ea4b0c6e527d07123ba
Romvet <v.romanenko@inspire.kiev.ua>
2019-01-03 11:10:00 +0200

    Исправления после предзащиты

--- js/script.js
+++ js/script.js
@@ -1,47 +1,54 @@
-var clickbutton= document.querySelector(".button-search");
-var modalform=document.querySelector(".modal-search");
-var form=modalform.querySelector("form");
-
-var checkin=modalform.querySelector("[name=date-check-in]");
-var checkout=modalform.querySelector("[name=date-check-out]");
-var numberpeople=modalform.querySelector("[name=number-people]");
-var numberchild=modalform.querySelector("[name=number-child]");
-
+var clickbutton = document.querySelector(".button-search");
+var modalform = document.querySelector(".modal-search");
+var form = modalform.querySelector("form");
+var checkin = modalform.querySelector("[name=date-check-in]");
+var checkout = modalform.querySelector("[name=date-check-out]");
+var numberpeople = modalform.querySelector("[name=number-people]");
+var numberchild = modalform.querySelector("[name=number-child]");
 var isStorageSupport = true;
 var storage = "";
 
-try{
+try {
   storage = localStorage.getItem("checkin");
-} catch (err){
+} catch (err) {
   isStorageSupport = false;
 }
 
- modalform.classList.add("modal-hid");
+modalform.classList.add("modal-hid");
 
- clickbutton.addEventListener("click", function (evt) {
+clickbutton.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalform.classList.toggle("modal-hid");
-   modalform.classList.toggle("modal-show");
-  if (storage){
+  modalform.classList.toggle("modal-show");
+  if (storage) {
     checkin.value = storage;
     checkout.focus();
-  } else{
+  } else {
     checkin.focus();
   }
-})
+});
 
-
-form.addEventListener("submit", function (evt) {
-if(!checkin.value || !checkout.value || !numberpeople.value || !numberchild.value){
-evt.preventDefault();
-console.log("Заполните пустые поля");
-  } else{
-    if (isStorageSupport){
-    localStorage.setItem("checkin", login.value);
+form.addEventListener("submit", function(evt) {
+  if (
+    !checkin.value ||
+    !checkout.value ||
+    !numberpeople.value ||
+    !numberchild.value
+  ) {
+    evt.preventDefault();
+    console.log("Заполните пустые поля");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("checkin", login.value);
     }
   }
 });
 
-
-
-
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (modalform.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalform.classList.remove("modal-show");
+    }
+  }
+});
